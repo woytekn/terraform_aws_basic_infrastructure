cb6d8ca9ea6dc188065fd11ee94c9711b3c59332
woytekn <wojciech.nartowski@netguru.com>
2024-07-08 14:19:15 +0200

    Elastic ip adjustments added

--- basicInfrstructure/terraform-manifests/c8-elasticip.tf
+++ basicInfrstructure/terraform-manifests/c8-elasticip.tf
@@ -2,6 +2,7 @@
 
 resource "aws_eip" "bastion_host" {
     depends_on = [ module.ec2_public_bastion_instance, module.vpc ]
-    instance = module.ec2_public_bastion_instance.id[0]
+    instance = module.ec2_public_bastion_instance.id
+    domain = "vpc"
     tags = local.common_tags
 }
